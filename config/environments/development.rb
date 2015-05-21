2dbf59f75c9ab1c30acd92e255a10a8cb95acf16
Kai Amundsen <amundsen.kai@gmail.com>
2015-05-20 21:02:45 -0500

    fixing depreciated config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = true
 end
